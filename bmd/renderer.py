45f50ca9aff77f31fdea5cc42377a0c6109da811
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2026-09-22 14:32:26 +0100

    math block now works, inline math is coming eventually

--- bmd/renderer.py
+++ bmd/renderer.py
@@ -138,8 +138,8 @@ def render_pagebreak(_lines: list[str]) -> str:
 
 @strip_lines
 @remove_empty_lines
-def render_math(_lines: list[str]) -> str:
-    return "<p>MATH UNSUPPORTED</p>"
+def render_math(lines: list[str]) -> str:
+    return f"<div class='math-block' data-latex='{formattedText(" ".join(lines))}></div>"
 
 
 def render_code(lines: list[str]) -> str:
